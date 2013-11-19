df7f60e6d30ba7f1e4d75ad9de61dd0f5e9068e4
Drew Blumberg <alblumberg21@gmail.com>
2013-11-19 09:56:45 -0600

    initial commit

---
+++ public/javascripts/app/app.js
@@ -0,0 +1,20 @@
+$(document).ready(initialize);
+
+var socket;
+
+function initialize(){
+  $(document).foundation();
+  initializeSocketIO();
+}
+
+function initializeSocketIO(){
+  var port = location.port ? location.port : '80';
+  var url = location.protocol + '//' + location.hostname + ':' + port + '/app';
+
+  socket = io.connect(url);
+  socket.on('connected', socketConnected);
+}
+
+function socketConnected(data){
+  console.log(data);
+}
